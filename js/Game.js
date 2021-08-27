05055d98894a400e089a8a4af6814c852f4f780a
FlowerSeerat <seerat3208@gmail.com>
2021-08-27 08:20:47 -0400

    reset button and leaderboard

--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,12 @@
 class Game {
   constructor() {
-    this.resetTitle = createElement("h2");
-    this.resetButton = createButton("");
+    this.resetTitle=createElement("h2");
+    this.resetButton=createButton("");
 
-    this.leadeboardTitle = createElement("h2");
-
-    this.leader1 = createElement("h2");
-    this.leader2 = createElement("h2");
+    this.leaderboardTitle = createElement("h2");
+    this.leader1=createElement("h2");
+    this.leader2 =createElement("h2");
+   
   }
 
   getState() {
@@ -52,21 +52,21 @@ class Game {
     this.resetButton.class("resetButton");
     this.resetButton.position(width / 2 + 230, 100);
 
-    this.leadeboardTitle.html("Leaderboard");
-    this.leadeboardTitle.class("resetText");
-    this.leadeboardTitle.position(width / 3 - 60, 40);
+    this.leaderboardTitle.html("Leaderboard");
+    this.leaderboardTitle.class("resetText");
+    this.leaderboardTitle.position(width/3-60,40);
 
     this.leader1.class("leadersText");
-    this.leader1.position(width / 3 - 50, 80);
+    this.leader1.position(width/3 - 50,80);
 
     this.leader2.class("leadersText");
-    this.leader2.position(width / 3 - 50, 130);
+    this.leader2.position(width/3 - 50,130);
   }
 
   play() {
     this.handleElements();
+   
     this.handleResetButton();
-
     Player.getPlayersInfo();
 
     if (allPlayers !== undefined) {
@@ -104,74 +104,77 @@ class Game {
     }
   }
 
-  handleResetButton() {
-    this.resetButton.mousePressed(() => {
-      database.ref("/").set({
-        playerCount: 0,
-        gameState: 0,
-        players: {}
-      });
-      window.location.reload();
-    });
-  }
-
   showLeaderboard() {
-    var leader1, leader2;
+    var leader1,leader2;
     var players = Object.values(allPlayers);
-    if (
-      (players[0].rank === 0 && players[1].rank === 0) ||
-      players[0].rank === 1
-    ) {
-      // &emsp;    This tag is used for displaying four spaces.
-      leader1 =
-        players[0].rank +
-        "&emsp;" +
-        players[0].name +
-        "&emsp;" +
-        players[0].score;
 
-      leader2 =
-        players[1].rank +
-        "&emsp;" +
-        players[1].name +
-        "&emsp;" +
-        players[1].score;
+    if(
+      (players[0].rank === 0 && players[1].rank ===0) ||
+      players[0].rank ===1
+    ) {
+        leader1 = 
+                  players[0].rank +
+                  "&emsp;" +
+                  players[0].name +
+                  "&emsp;" +
+                  players[0].score;
+
+        leader2 = 
+                  players[1].rank +
+                  "&emsp;" +
+                  players[1].name +
+                  "&emsp;" +
+                  players[1].score;
     }
 
-    if (players[1].rank === 1) {
-      leader1 =
-        players[1].rank +
-        "&emsp;" +
-        players[1].name +
-        "&emsp;" +
-        players[1].score;
+    if(players[1].rank === 1) {
+      leader1 = 
+                  players[1].rank +
+                  "&emsp;" +
+                  players[1].name +
+                  "&emsp;" +
+                  players[1].score;
 
-      leader2 =
-        players[0].rank +
-        "&emsp;" +
-        players[0].name +
-        "&emsp;" +
-        players[0].score;
+        leader2 = 
+                  players[0].rank +
+                  "&emsp;" +
+                  players[0].name +
+                  "&emsp;" +
+                  players[0].score;
     }
 
     this.leader1.html(leader1);
     this.leader2.html(leader2);
   }
 
+
   handlePlayerControls() {
     if (keyIsDown(UP_ARROW)) {
       player.positionY += 10;
       player.update();
     }
-
-    if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50) {
-      player.positionX -= 5;
+    if(keyIsDown(LEFT_ARROW)&&player.positionX>width/3-50){
+      player.positionX-=5;
+      player.update();
+    }
+    if(keyIsDown(RIGHT_ARROW)&&player.positionX<width/2+300){
+      player.positionX+=5;
       player.update();
     }
 
-    if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300) {
-      player.positionX += 5;
-      player.update();
-    }
+  
+  }
+
+  handleResetButton(){
+    this.resetButton.mousePressed(()=>{
+      database.ref("/").set({
+        playerCount:0,
+        gameState:0,
+        players:{}
+      })
+      window.location.reload();
+    })
   }
 }
+
+
